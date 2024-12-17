97d0da8f69b94909cbb166cd3325e780b8aad68a
IF <139582705+infiniteflower@users.noreply.github.com>
2024-12-17 17:45:32 -0500

    chore: calc usd value in addition to display currency value

--- ui/ducks/bridge/selectors.ts
+++ ui/ducks/bridge/selectors.ts
@@ -321,12 +321,14 @@ const _getQuotesWithMetadata = createSelector(
   getToTokenConversionRate,
   getFromTokenConversionRate,
   getConversionRate,
+  getUSDConversionRate,
   _getBridgeFeesPerGas,
   (
     quotes,
     toTokenExchangeRate,
     fromTokenExchangeRate,
-    nativeExchangeRate,
+    nativeToDisplayCurrencyExchangeRate,
+    nativeToUsdExchangeRate,
     {
       estimatedBaseFeeInDecGwei,
       maxPriorityFeePerGasInDecGwei,
@@ -344,29 +346,37 @@ const _getQuotesWithMetadata = createSelector(
         estimatedBaseFeeInDecGwei,
         maxFeePerGasInDecGwei,
         maxPriorityFeePerGasInDecGwei,
-        nativeExchangeRate,
+        nativeToDisplayCurrencyExchangeRate,
+        nativeToUsdExchangeRate,
       });
-      const relayerFee = calcRelayerFee(quote, nativeExchangeRate);
+      const relayerFee = calcRelayerFee(
+        quote,
+        nativeToDisplayCurrencyExchangeRate,
+        nativeToUsdExchangeRate,
+      );
       const totalEstimatedNetworkFee = {
         amount: gasFee.amount.plus(relayerFee.amount),
         valueInCurrency:
           gasFee.valueInCurrency?.plus(relayerFee.valueInCurrency || '0') ??
           null,
+        usd: gasFee.usd?.plus(relayerFee.usd || '0') ?? null,
       };
       const totalMaxNetworkFee = {
         amount: gasFee.amountMax.plus(relayerFee.amount),
         valueInCurrency:
           gasFee.valueInCurrencyMax?.plus(relayerFee.valueInCurrency || '0') ??
           null,
+        usd: gasFee.usdMax?.plus(relayerFee.usd || '0') ?? null,
       };
 
       const sentAmount = calcSentAmount(
         quote.quote,
         fromTokenExchangeRate.valueInCurrency,
+        fromTokenExchangeRate.usd,
       );
       const adjustedReturn = calcAdjustedReturn(
-        toTokenAmount.valueInCurrency,
-        totalEstimatedNetworkFee.valueInCurrency,
+        toTokenAmount,
+        totalEstimatedNetworkFee,
       );
 
       return {
@@ -380,10 +390,7 @@ const _getQuotesWithMetadata = createSelector(
         adjustedReturn,
         gasFee,
         swapRate: calcSwapRate(sentAmount.amount, toTokenAmount.amount),
-        cost: calcCost(
-          adjustedReturn.valueInCurrency,
-          sentAmount.valueInCurrency,
-        ),
+        cost: calcCost(adjustedReturn, sentAmount),
       };
     });
 
